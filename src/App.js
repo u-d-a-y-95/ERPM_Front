406f71b51c84081453092b88ca970dc12eb8d0f9
touhiduzzaman-tuhin <touhiduzzamantuhin95@gmail.com>
2021-04-13 16:08:48 +0600

    Login Page initial stage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import http from './main/services/http/http-client'
-import ls from './main/services/local-storage'
+import logo from "./logo.svg";
+import "./App.css";
+import http from "./main/services/http/http-client";
+import ls from "./main/services/local-storage";
 // import {TOKEN,TOKEN1} from './main/constant/local-storage.contant'
 import MasterInput from './main/common/base-component/master-input'
 import LoginInput from './main/common/base-component/login-input'
 import { useState } from 'react';
+import MasterInput from "./main/common/base-component/master-input";
+import { useState } from "react";
+import akijCement from "./assets/image/login-images/Artboard 11 copy 15@2x.png";
+import assl from "./assets/image/login-images/ASSL.png";
+import artboard from "./assets/image/login-images/Artboard 11 copy 10.png";
+import artboard11 from "./assets/image/login-images/Artboard 11.png";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { useFormik } from "formik";
 
 function App() {
   // http.de('https://jsonplaceholder.typicode.com/todos/1')
@@ -32,6 +41,12 @@ function App() {
       </div>
     </div>
   );
+  // const [name, setName] = useState("")
+
+  // function onChange(e) {
+  //   setName(e.target.value);
+  // }
+  return null;
 }
 
 export default App;
