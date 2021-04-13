86f64b6a8c7bd2db2899c64a9d734480b349c5bd
Emdadul Haque <emdadibos@gmail.com>
2021-04-13 11:48:01 +0600

    add file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import http from './main/services/http/http-client'
-import ls from './main/services/local-storage'
+import logo from "./logo.svg";
+import "./App.css";
+import http from "./main/services/http/http-client";
+import ls from "./main/services/local-storage";
 // import {TOKEN,TOKEN1} from './main/constant/local-storage.contant'
-import MasterInput from './main/common/base-component/master-input'
-import { useState } from 'react';
+import MasterInput from "./main/common/base-component/master-input";
+import { useState } from "react";
 
 function App() {
   // http.de('https://jsonplaceholder.typicode.com/todos/1')
@@ -12,9 +12,10 @@ function App() {
   //   console.log(res)
   // })
   // ls.setData(TOKEN,'aziz')
-  const [name,setName] = useState("")
-  function onChange (e) {
-    setName(e.target.value)
+  const [name, setName] = useState("");
+  function onChange(e) {
+    setName(e.target.value);
+    console.log("dasdsd");
   }
   return (
     <div>
@@ -24,7 +25,7 @@ function App() {
         placeHolder="Enter your name"
         type="text"
         value={name}
-        onChange = {onChange}
+        onChange={onChange}
       />
     </div>
   );
