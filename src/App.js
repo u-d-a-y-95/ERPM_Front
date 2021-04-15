f9c3981c85ef0d3f703a4c5d7d2b5ec43f4531ba
Emdadul Haque <emdadibos@gmail.com>
2021-04-16 00:58:28 +0600

    sign up page done with responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import SignUp from "./main/base-layout/Screens/SignUp";
-import Test from "./main/base-layout/Screens/Test";
 
 function App() {
   return (
     <div>
-      {/* <SignUp /> */}
-      <Test />
+      <SignUp />
     </div>
   );
 }
