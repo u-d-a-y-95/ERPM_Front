428c6dc01823751fa1958f0729b1a84cc00786a2
touhiduzzaman-tuhin <touhiduzzamantuhin95@gmail.com>
2021-04-15 16:53:22 +0600

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-import http from "./main/services/http/http-client";
-import ls from "./main/services/local-storage";
-// import {TOKEN,TOKEN1} from './main/constant/local-storage.contant'
-import MasterInput from './main/common/base-component/master-input'
-import LoginInput from './main/common/base-component/login-input'
-import { useState } from 'react';
-import MasterInput from "./main/common/base-component/master-input";
-import { useState } from "react";
-import akijCement from "./assets/image/login-images/Artboard 11 copy 15@2x.png";
-import assl from "./assets/image/login-images/ASSL.png";
-import artboard from "./assets/image/login-images/Artboard 11 copy 10.png";
-import artboard11 from "./assets/image/login-images/Artboard 11.png";
-import { Formik } from "formik";
-import * as Yup from "yup";
-import { useFormik } from "formik";
+// import http from "./main/services/http/http-client";
+// import ls from "./main/services/local-storage";
+// // import {TOKEN,TOKEN1} from './main/constant/local-storage.contant'
+// import MasterInput from './main/common/base-component/master-input'
+// import LoginInput from './main/common/base-component/login-input'
+// import { useState } from 'react';
+// import MasterInput from "./main/common/base-component/master-input";
+// import { useState } from "react";
+// import akijCement from "./assets/image/login-images/Artboard 11 copy 15@2x.png";
+// import assl from "./assets/image/login-images/ASSL.png";
+// import artboard from "./assets/image/login-images/Artboard 11 copy 10.png";
+// import artboard11 from "./assets/image/login-images/Artboard 11.png";
+// import { Formik } from "formik";
+// import * as Yup from "yup";
+// import { useFormik } from "formik";
+import Login from "./main/base-layout/Login/Login";
 
 function App() {
   // http.de('https://jsonplaceholder.typicode.com/todos/1')
@@ -22,31 +23,34 @@ function App() {
   //   console.log(res)
   // })
   // ls.setData(TOKEN,'aziz')
-  const [name, setName] = useState("")
-  function onChange(e) {
-    setName(e.target.value)
-  }
+  // const [name, setName] = useState("")
+  // function onChange(e) {
+  //   setName(e.target.value)
+  // }
   return (
-    <div class="row">
-      <div className="col-md-3 mx-5">
-       <LoginInput 
-          name="email"
-          label="Email"
-          type="text"
-          value=""
-          onChange=""
-          placeHolder=""
-          icon="fa fa-envelope"
-       />
-      </div>
+    <div>
+      <Login />
     </div>
+    // <div class="row">
+    //   <div className="col-md-3 mx-5">
+    //    <LoginInput 
+    //       name="email"
+    //       label="Email"
+    //       type="text"
+    //       value=""
+    //       onChange=""
+    //       placeHolder=""
+    //       icon="fa fa-envelope"
+    //    />
+    //   </div>
+    // </div>
   );
   // const [name, setName] = useState("")
 
   // function onChange(e) {
   //   setName(e.target.value);
   // }
-  return null;
+  // return null;
 }
 
 export default App;
