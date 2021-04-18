2b0efe3cf119975a64fef8e0f44a075c5d6f6a2f
UDay <saiful@ibos.io>
2021-04-18 13:46:17 +0600

    fixed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import SignUp from "./main/base-layout/Screens/SignUp";
 import Login from "./main/base-layout/Screens/Login";
 import "./App.css";
+import MainLayout from "./main/base-layout/main-layout";
 function App() {
   return (
     <div>
       {/* <SignUp /> */}
-      <Login />
+      {/* <Login /> */}
+      <MainLayout />
     </div>
   );
 }
--- src/main/base-layout/sidebar.js
+++ src/main/base-layout/sidebar.js
@@ -3,7 +3,8 @@ import NavLink from '../common/base-component/nav-link'
 import MasterSelect from '../common/base-component/master-select'
 import { menu } from '../constant/url.constant'
 import '../../assets/css/sidebar.css'
-import logo from "../../assets/image/iboslogo.png"
+import logo from "../../assets/image/erp-logo.png"
+import ibosIcon from "../../assets/image/ibosIcon.png"
 function Sidebar(props) {
     function onClickHandler(e) {
         console.dir(e.target.parentNode)
@@ -15,9 +16,28 @@ function Sidebar(props) {
         }
     }
     return (
-        <div className="sidebar" style={props.isExpandSidebar ? {width:"250px"} : {width:"70px"}}>
-            <div className="company-logo  mt-3">
-                    <img src={logo}/>
+        <div className="sidebar" style={props.isExpandSidebar ? { width: "250px" } : { width: "70px" }}>
+            <div className="company-logo d-flex justify-content-center  mt-3">
+                {
+                    props.isExpandSidebar &&
+                    <img
+                        style={{
+                            width: "150px",
+                            transition:"width" 
+                        }}
+                        src={logo}
+                        alt="ibosLOg"
+                    />
+                    ||
+                    < img
+                        style={{
+                            width: "25px"
+                        }}
+                        src={ibosIcon}
+                        alt="ibosLOg"
+                    />
+
+                }
 
             </div>
             <div className="branchSelect">
@@ -30,7 +50,7 @@ function Sidebar(props) {
                             <div className={item.children.length > 0 ? "menu-li test" : "menu-li"}>
                                 <span className="menu-li-link" onClick={onClickHandler}>
                                     <i className={`${item.icon} mr-3`}></i>
-                                    { item.label}</span>
+                                    {item.label}</span>
                                 {
                                     item.children && <NavLink items={item.children} />
                                 }
