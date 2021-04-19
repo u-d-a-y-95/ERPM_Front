46c724453fa4009ad6b5729b0af86b5f6c37948d
touhiduzzaman-tuhin <touhiduzzamantuhin95@gmail.com>
2021-04-19 14:54:11 +0600

    Item Page

--- src/main/module/Item/Item-Category/ItemCategory.js
+++ src/main/module/Item/Item-Category/ItemCategory.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import ItemCategoryForm from "./Form";
 import { getList } from "./http";
@@ -43,3 +44,49 @@ function ItemCategory() {
 }
 
 export default ItemCategory;
+=======
+import React, { useState, useEffect } from 'react'
+import ItemCategoryForm from './Form';
+import { getList } from './http'
+// import { getList, purchaseOrderDeleteData } from './http'
+import ItemCategoryTable from './Table';
+
+function ItemCategory() {
+    const [tableData, setTableData] = useState([])
+    const [upDate, setUpData] = useState({})
+    useEffect(() => {
+        populateTable()
+    }, [])
+    const populateTable = () => {
+        getList(setTableData)
+    }
+    // const deleteFromTable = (id) => {
+    //     purchaseOrderDeleteData(id, populateTable)
+    // }
+    const updateToTable = (row) => {
+        // console.log(row)
+        row['description'] = row['body']
+        setUpData(row)
+    }
+    return (
+        <>
+            <h3 className="">
+                Item Category
+            </h3>
+            <ItemCategoryForm
+                populateTable={populateTable}
+                upDate={upDate}
+                setUpData={setUpData}
+            />
+            <ItemCategoryTable
+                data={tableData}
+                // deleteFromTable={deleteFromTable}
+                updateToTable={updateToTable}
+            />
+        </>
+    )
+}
+
+
+export default ItemCategory
+>>>>>>> 28c1f79 (Item Page)
--- src/main/module/Item/Item-Category/http.js
+++ src/main/module/Item/Item-Category/http.js
@@ -148,3 +148,26 @@ export const getItemTypeDropdownListAction = (setter) => {
       console.log("Error", error?.message);
     });
 };
+import httpClient from "../../../services/http/http-client"
+
+export const createItemCategory = (payload, formik, populateTable) => {
+    const obj = createPayloadChange(payload);
+    httpClient.postData('https://jsonplaceholder.typicode.com/user', obj)
+        .then(res => {
+
+            formik.resetForm();
+            populateTable()
+        })
+
+}
+export const getList = (setData) => {
+    httpClient.getData('https://jsonplaceholder.typicode.com/users')
+        .then(res => {
+            setData(res.data)
+        })
+
+}
+
+const createPayloadChange = (payload) => {
+    return payload
+}
