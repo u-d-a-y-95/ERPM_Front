e11d477c62635f5a00ade9130b712311b8a6503d
touhiduzzaman-tuhin <touhiduzzamantuhin95@gmail.com>
2021-04-19 14:54:11 +0600

    Item Page

--- src/main/base-layout/holder.js
+++ src/main/base-layout/holder.js
@@ -2,25 +2,22 @@ import React, { useState, useEffect } from "react";
 import "../../assets/css/holder.css";
 import BusinessUnit from "../module/Purchase/Business-Unit/BusinessUnit";
 
-import PurchaseOrder from "../module/Purchase/Purchase-Order/PurchaseOrder";
-import Supplier from "../module/Purchase/Supplier/Supplier";
-import Customer from "../module/Purchase/Customer/Customer";
-import ItemProfile from './../module/Item/Item-Profile/ItemProfile';
+import ItemProfile from '../module/Item/Item-Profile/ItemProfile'
+import ItemSubCategory from '../module/Item/Item-Sub-Category/ItemSubCategory';
+
+import PurchaseOrder from '../module/Purchase/Purchase-Order/PurchaseOrder'
 import ItemCategory from './../module/Item/Item-Category/ItemCategory';
-import ItemSubCategory from './../module/Item/Item-Sub-Category/ItemSubCategory';
+
 
 function Holder() {
-  return (
-    <div className='holder h-100 p-5'>
-      {/* <PurchaseOrder /> */}
-      {/* <BusinessUnit /> */}
-      {/* <Supplier /> */}
-      {/* <Customer /> */}
-      <ItemProfile />
-      {/* <ItemCategory /> */}
-      {/* <ItemSubCategory /> */}
-    </div>
-  );
+    return (
+        <div className="holder h-100 p-5">
+          {/* <PurchaseOrder /> */}
+          <ItemProfile />
+          {/* <ItemCategory /> */}
+          {/* <ItemSubCategory /> */}
+        </div>
+    )
 }
 
 export default Holder;
