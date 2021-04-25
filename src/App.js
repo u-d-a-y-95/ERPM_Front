a7a52765e184ddda83d9fdeeccd6560f4fee4f38
azizul016 <haqueazizul789@gmail.com>
2021-04-25 10:17:11 +0600

    changes

--- src/App.js
+++ src/App.js
@@ -3,23 +3,17 @@ import SignUp from "./main/base-layout/Screens/SignUp";
 import Login from "./main/base-layout/Screens/Login";
 import "./App.css";
 import MainLayout from "./main/base-layout/main-layout";
-import { ToastContainer } from 'react-toastify'
+import { ToastContainer } from "react-toastify";
 import { useSelector } from "react-redux";
 
-
 function App() {
-  const isLogged = useSelector(state => state.auth.isLogged)
+  const isLogged = useSelector((state) => state.auth.isLogged);
 
   return (
     <div>
       {/* <SignUp /> */}
       {/* <Login /> */}
-      {
-        isLogged &&
-        <MainLayout />
-        ||
-        <Login />
-      }
+      {(isLogged && <MainLayout />) || <Login />}
       <ToastContainer />
     </div>
   );
