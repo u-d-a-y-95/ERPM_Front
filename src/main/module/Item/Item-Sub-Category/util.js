a86a9acc7a56b5ae3a7c51aa1062a0da2803cf77
touhiduzzaman-tuhin <touhiduzzamantuhin95@gmail.com>
2021-04-26 11:28:12 +0600

    Item Profile Update Done

--- src/main/module/Item/Item-Sub-Category/util.js
+++ src/main/module/Item/Item-Sub-Category/util.js
@@ -1,42 +1,69 @@
-import * as Yup from "yup"
+import * as Yup from "yup";
 
 export const initialValues = {
-    businessUnit: "",
-    subCategory : "",
-    itemType: "",
-    category: "",
-}
-
+  businessUnit: "",
+  subCategory: "",
+  itemType: "",
+  category: "",
+};
 
 export const tableConfig = {
-    headers: [
-        {
-            key: "sl", label : 'SL'
-        },
-        {
-            key: "itemSubCategoryName", label: "Sub-Category"
-        },
-        {
-            key: "itemTypeName", label: "Item Type"
-        },
-        {
-            key: "itemCategoryName", label: "Category"
-        },
-        {
-            key: "businessUnitId", label: "Business Unit"
-        },
-    ],
-    isSearchable: true
-}
-
+  headers: [
+    {
+      key: "sl",
+      label: "SL",
+    },
+    {
+      key: "itemSubCategoryName",
+      label: "Sub-Category",
+    },
+    {
+      key: "itemTypeName",
+      label: "Item Type",
+    },
+    {
+      key: "itemCategoryName",
+      label: "Category",
+    },
+    {
+      key: "businessUnitId",
+      label: "Business Unit",
+    },
+  ],
+  isSearchable: true,
+};
 
 export const formsValidationSchema = Yup.object().shape({
-    subCategory: Yup.string().required("Sub Category is required"),
-    category: Yup.object().shape({
-        value: Yup.string().required("Category is required"),
-      }),
-    itemType: Yup.object().shape({
-      value: Yup.string().required("Item Type is required"),
-    }),
-    businessUnit: Yup.string().required("Business Uint is required"),
-  });
+  subCategory: Yup.string().required("Sub Category is required"),
+  category: Yup.object().shape({
+    value: Yup.string().required("Category is required"),
+  }),
+  itemType: Yup.object().shape({
+    value: Yup.string().required("Item Type is required"),
+  }),
+  businessUnit: Yup.string().required("Business Uint is required"),
+});
+
+
+export const itemProfileViewUpdatePayloadData = (value) => {
+    console.log(value)
+    const payload = {
+      businessUnit: value?.businessUnitId,
+      category: {
+        value: value?.itemCategoryId,
+        label: value?.itemCategoryName,
+      },
+      subCategory: {
+        value: value?.itemSubCategoryId,
+        label: value?.itemSubCategoryName,
+      },
+      itemType: {
+        value: value?.itemTypeId,
+        label: value?.itemTypeName,
+      },
+      sl: value?.sl,
+  
+    };
+    return payload;
+  };
+  
